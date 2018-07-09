d91870a2ea2351299fd4da6083941de4ed60c61d
ATUL KUMAR <atulrulers@gmail.com>
2018-07-10 01:49:40 +0530

    Add files via upload

---
+++ biased_spoj_greedy.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+
+ll minBadness(vector < ll > v){
+    ll s = v.size();
+
+    sort(v.begin(),v.end()); // sort the rank value
+
+    ll ans = 0;
+    for(ll i=0;i<s;i++){ // start assigning rank to each one
+        ans = ans + abs(v[i] - i - 1); // calculate absolute difference between the possible rank and desired rank
+    }
+
+    return ans;
+}
+
+int main(){
+    ll t;
+    cin>>t; // number of test cases
+
+    while(t--){
+        vector< ll > v;
+        ll n ;
+        cin >> n ;
+        string s;
+        ll temp;
+        for(ll i=0;i<n;i++){
+            cin>>s>>temp;
+            v.push_back(temp);
+        }
+        cout<<minBadness(v)<<endl;
+    }
+
+    return 0;
+
+}
